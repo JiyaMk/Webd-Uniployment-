54010cc57b6521816348a929d6a257895038454c
JiyaMk <jiya.mk.64@gmail.com>
2023-07-31 22:32:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function submitForm() {
+    // Get selected day and time values
+    var selectedDay = document.getElementById("day").value;
+    var selectedTime = document.getElementById("time").value;
+  
+    // Perform actions with selected values
+    console.log("Selected day: " + selectedDay);
+    console.log("Selected time: " + selectedTime);
+    // You can replace the above console.log statements with your desired logic or actions
+  }
+
+  function myhome(){
+    location.replace("homepage.html")
+  }
+  
